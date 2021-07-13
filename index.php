afb6309b168c54c126cd2d455a794ad13d145fa2
ashik4715 <ashikurjhalak@gmail.com>
2021-07-13 18:22:53 +0200

    refactoring exiting code

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $username = "root";
 $password = "password";
 $dbname = "rexx";
 $salesController = new SalesController($servername, $username, $password, $dbname);
+$connect = new mysqli($servername, $username, $password, $dbname);
 
 class SalesController
 {
@@ -27,13 +28,7 @@ class SalesController
     {
         $this->conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        } else {
-            echo "Database successfully connected. \n";
-        }
-        
-        $sql = "CREATE TABLE BookShop (
+        $sql = "CREATE TABLE bookshop (
             sale_id MEDIUMINT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             customer_name VARCHAR(100) NOT NULL,
             customer_mail VARCHAR(100) NOT NULL,
@@ -47,18 +42,105 @@ class SalesController
             echo "Table BookShop created successfully \n";
         } else {
             echo "Error creating table: " . $this->conn->error;
+            
         }
     }
 
-    function read(){
+    function read()
+    {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // collect value of input field
             $name = $_POST['fname'];
             if (empty($name)) {
-              echo "Name is empty";
+                echo "Name is empty";
             } else {
-              echo $name;
+                echo $name;
             }
-          }
+        }
     }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rexx Systems Coding Challange</title>
+
+    <style>
+        #shop {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #shop td,
+        #shop th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+        }
+
+        #shop tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #shop tr:hover {
+            background-color: #ddd;
+        }
+
+        #shop th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
+
+</head>
+
+<body>
+    <?php
+    /**
+     * database query for displaying all rows into html table
+     * @param sql1
+     */
+    $sql1 = "SELECT * from `bookshop`;";
+    $result1 = $connect->query($sql1);
+    while ($rows = $result1->fetch_array()) {
+    ?>
+        <table id="shop" style="width:100%">
+            <tr>
+                <th>Sale ID</th>
+                <th>Customer Name</th>
+                <th>Customer Mail</th>
+                <th>Product Name</th>
+                <th>Product ID</th>
+                <th>Product Price</th>
+                <th>Sale Date</th>
+            </tr>
+            <tr>
+                <td><?php echo $rows["sale_id"] ?></td>
+                <td><?php echo $rows["customer_name"] ?></td>
+                <td><?php echo $rows["customer_mail"] ?></td>
+                <td><?php echo $rows["product_name"] ?></td>
+                <td><?php echo $rows["product_id"] ?></td>
+                <td><?php echo $rows["product_price"] ?></td>
+                <td><?php echo $rows["sale_date"] ?></td>
+            </tr>
+            <tr>
+                <td colspan="7">Total Price : </td>
+            </tr>
+        </table>
+
+</body>
+
+</html>
+<?php
+    }
+?>
